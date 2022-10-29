0491f479a4d1bbb2ad9cf7cf2a8e186397f17953
Nabaa-Munaf <nabaamunaf3@gmail.com>
2022-10-29 19:55:57 +0300

    slove 1

---
+++ 1/1.dart
@@ -0,0 +1,24 @@
+/*
+write a variable in each of below value type and print its value (try to not use 'var/final/const' keyword)
+the final output may be like this
+
+true
+4
+1.2
+ok
+[4,3,"ok",true]
+
+ */
+
+void main(List<String> args) {
+  bool boolean=true;
+  int number=4;
+  double doubleNumber=1.2;
+  String word="ok";
+  List myList=[4,3,"ok",true];
+  print(boolean);
+  print(number);
+  print(doubleNumber);
+  print(word);
+  print(myList);
+  }
---
+++ 1/2.dart
@@ -0,0 +1,35 @@
+/*
+complete the below code using relational and arithmetic operator
+ */
+
+void main(List<String> args) {
+  int x = 5;
+  int y = 10;
+
+  print("is x equal y:");
+  print(x==y);
+
+  print("is x bigger than y:");
+  print(x>y);
+
+  print("is x not equal y:");
+  print(x!=y);
+
+  print("is x less than or equal y:");
+  print(x<=y);
+
+  print("the value of 5 plus 2 is");
+  print(5+2);
+
+  print("the value of 3 multiply 6 is");
+  print(3*6);
+
+  print("the value of 5 divide by 2 is");
+  print(5/2);
+
+  print("the integer value of 5 divide by 2 is");
+  print(5~/2);
+
+  print("is x between 10 and 30");
+  print(x>10 && x<30);
+}
